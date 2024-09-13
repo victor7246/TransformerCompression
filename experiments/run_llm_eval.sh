000bf3236c05ced7eb17845bb1a295b262eaee4d
Siddhant Chaudhary <urssidd@gmail.com>
2024-09-14 03:17:20 +0530

    Updating the running commands.

--- experiments/run_llm_eval.sh
+++ experiments/run_llm_eval.sh
@@ -1,9 +1,21 @@
+# slice with RL model
 TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2  python run_lm_eval.py  \
     --model facebook/opt-125m     \
     --sparsity 0.2        \
     --tasks hellaswag arc_easy arc_challenge winogrande piqa      \
     --no-wandb  \
     --use-slicing \
+    --slice_with_action_model \
+    --finetune
+
+# slice with uniform sampling
+TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2  python run_lm_eval.py  \
+    --model facebook/opt-125m     \
+    --sparsity 0.2        \
+    --tasks hellaswag arc_easy arc_challenge winogrande piqa      \
+    --no-wandb  \
+    --use-slicing \
+    --slice_uniform \
     --finetune
 
 TF_CPP_MIN_LOG_LEVEL=2 TF_ENABLE_ONEDNN_OPTS=1 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2  python run_lm_eval.py  \
